2cd5a8e59f418df26ce854f136996ebc2c31cd4e
mustapha bousfina <bousfinamustapha@gmail.com>
2023-06-16 15:02:08 -0700

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
