269565061ac354bf3428583077209e0d5d7fa00d
mustapha bousfina <bousfinamustapha@gmail.com>
2023-06-21 10:16:56 -0700

    second task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * print_alphabet - utilizes on the _putchar function to print
@@ -6,9 +6,10 @@ include "main.h"
 */
 
 void print_alphabet(void)
-	
+{
 	int ch;
-	
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
+}
