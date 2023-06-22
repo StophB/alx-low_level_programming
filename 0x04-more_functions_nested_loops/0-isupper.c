ed674eea01c9b03ec750e0cd8e02cff8297a35f6
mustapha bousfina <bousfinamustapha@gmail.com>
2023-06-22 09:50:08 -0700

    done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
-* _isdigit - write a function that check for a digit (0 through 9).
+* _isdigit - write a function that check for a digit (0 - 9).
 * @c: char to check
 * Return:  0 or 1
 **/
+
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
