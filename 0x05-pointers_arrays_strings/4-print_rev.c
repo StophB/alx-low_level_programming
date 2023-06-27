da9718d096d0aa3aecd7eced70554fc4b6b621a3
mustapha bousfina <bousfinamustapha@gmail.com>
2023-06-27 06:28:29 -0700

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,11 @@
 #include "main.h"
+
 /**
- * rev_string - reverses a string
- * _putchar - print each character
- * @s: char to check
- *
- * Description: This will reverse a string
- * Return: 0 is success
- */
-void rev_string(char *s)
+* print_rev -> printing a string in reverse
+* @s: the string to be printed in rev
+*/
+
+void print_rev(char *s)
 {
 	int i = 0;
 
